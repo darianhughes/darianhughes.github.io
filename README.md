b8a091833993a6d349579453f8d1b4f87871f871
Darian Hughes <darianmhughes7@gmail.com>
2024-05-08 18:02:58 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Darians's Online Profile
+
+## About 
+
+Hi there! I'm Darian, a freshman in college. I'm passionate about codding and this is my online profile. 
---
+++ script.js
@@ -0,0 +1,127 @@
+$('#menuClick').on('change', function() {
+    if ($('#menuClick').is(':checked')) {
+        $('.menuContainer').css('left', '50%');
+        $('.menuContainer').css('box-shadow', '20px 0 125px #000');
+        $('#header').css('filter', 'blur(15px)', 'filter', 'brightness(50%)');
+    } else {
+        $('.menuContainer').css('left', '100%');
+        $('.menuContainer').css('box-shadow', '0px 0 0px #000');
+        $('#header').css('filter', 'blur(0px)', 'filter', 'brightness(100%)');
+    }
+});
+
+$(document).on('mousemove', function(e){
+    $('#cursor').css({
+       left:  e.pageX,
+       top:   e.pageY
+    });
+});
+
+$('#menuIcon').on('mouseover', function(){
+    $('#cursor').css({
+        width: '60px',
+        height: '60px'
+    })
+})
+
+$('#menuIcon').on('mouseleave', function(){
+    $('#cursor').css({
+        width: '10px',
+        height: '10px'
+    })
+})
+
+$('.menuLink').on('mouseover', function(){
+    $('#cursor').css({
+        width: '0px',
+        height: '0px'
+    })
+})
+
+$('.menuLink').on('mouseleave', function(){
+    $('#cursor').css({
+        width: '10px',
+        height: '10px'
+    })
+})
+
+$('.contact').on('mouseover', function(){
+    $('#cursor').css({
+        width: '0px',
+        height: '0px'
+    })
+})
+
+$('.contact').on('mouseleave', function(){
+    $('#cursor').css({
+        width: '10px',
+        height: '10px'
+    })
+})
+
+$('#emailUsLink').on('mouseover', function(){
+    $('#cursor').css({
+        width: '30px',
+        height: '30px'
+    })
+})
+
+$('#emailUsLink').on('mouseleave', function(){
+    $('#cursor').css({
+        width: '10px',
+        height: '10px'
+    })
+})
+
+$(document).on('mousedown', () => {
+    $('#cursor').css({
+        transform: 'scale(1.5) translate(-25%,-25%)'
+    })
+})
+
+$(document).on('mouseup', () => {
+    $('#cursor').css({
+        transform: 'scale(1) translate(-50%,-50%)'
+    })
+})
+
+$(document).on('mousemove', e => {
+    $('#bubble:nth-child(1)').css({
+        top: (50 + e.pageY/300 + '%'),
+        left: (50 + (e.pageX)/300 + '%')
+    })
+
+    $('#bubble:nth-child(2)').css({
+        top: (40 + e.pageY/150 + '%'),
+        left: (40 + (e.pageX)/175 + '%')
+    })
+
+    $('#bubble:nth-child(3)').css({
+        top: (60 + e.pageY/450 + '%'),
+        left: (60 - (e.pageX)/400 + '%')
+    })
+
+    $('#bubble:nth-child(4)').css({
+        top: (40 - e.pageY/200 + '%'),
+        left: (60 - (e.pageX)/350 + '%')
+    })
+
+    $('#bubble:nth-child(5)').css({
+        top: (60 - e.pageY/300 + '%'),
+        left: (40 + (e.pageX)/200 + '%')
+    })
+
+    $('#bubble:nth-child(6)').css({
+        top: (40 - e.pageY/350 + '%'),
+        left: (50 + (e.pageX)/450 + '%')
+    })
+
+    $('#bubble:nth-child(7)').css({
+        top: (60 + e.pageY/350 + '%'),
+        left: (50 + (e.pageX)/450 + '%')
+    })
+
+    $('#bigLetter').css({
+        left: (80 - e.pageX/400 + '%')
+    })
+})
